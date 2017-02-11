bbae7eadd63df4f0b144fcd9d13013be41492e84
Unknown <little_miss_kris07@hotmail.com>
2017-02-10 18:47:20 -0700

    Spinosaurus Animations

---
+++ Spinosaurus Animations/Assets/Assets/SpinosaurAnimationScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinosaurAnimationScript : MonoBehaviour 
+{
+    private Animator anim;
+    float timer = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timer <= 20)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= 0 && timer <= 5)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else if(timer > 5 && timer <= 10)
+        {
+            anim.SetBool("Run", true);
+        }
+        else if(timer > 10 && timer <= 15)
+        {
+            anim.SetBool("Jump", true);
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Jump", false);
+            anim.SetBool("Run", false);
+            anim.SetBool("Walk", false);
+        }
+	}
+}
